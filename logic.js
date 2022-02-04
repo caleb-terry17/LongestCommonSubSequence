16893a2fadbada7e6c6cdee3258f5be81f793029
caleb-terry17 <k.cterry30@gmail.com>
2022-02-04 01:17:06 -0600

    setup layout for displaying comparisons

--- logic.js
+++ logic.js
@@ -1,3 +1,16 @@
+// document tags to display compare values
+let tag1 = document.getElementById("tag1");
+let tag2 = document.getElementById("tag2");
+
+// to hold value of style sheet
+let style = document.documentElement.style;
+
+// color codes
+let green = "#09FF09";
+let yellow = "#FFFF00";
+let red = "FF0000";
+let white = "#FFF";
+
 function getInput() {
     // get input from list1, list2
     let str1 = document.getElementById("list1").value;
@@ -15,21 +28,29 @@ function getInput() {
     console.log(lcs(list1, list2, list1.length - 1, list2.length - 1));
 }
 
-let iter = 0;
-
 // longest common subsequence
 function lcs(l, r, lIdx, rIdx) {
-    iter++;
     // idx == -1 => end of check
     if (lIdx == -1 || rIdx == -1) { return 0; }
+    // setting outside values
+    tag1.innerHTML = l[lIdx];
+    tag2.innerHTML = r[rIdx];
     // val's == => add one, return and decrement both
     let total;
-    if (l[lIdx] == r[rIdx]) { total = lcs(l, r, lIdx - 1, rIdx - 1) + 1; }
+    if (l[lIdx] == r[rIdx]) { 
+        // lcs computation
+        total = lcs(l, r, lIdx - 1, rIdx - 1) + 1;
+        // displaying comparison
+        style.setProperty("--color2", green);
+    }
     // return greater of two
     else {
+        // lcs computation
         let lhs = lcs(l, r, lIdx - 1, rIdx);
         let rhs = lcs(l, r, lIdx, rIdx - 1);
         total = (lhs > rhs) ? lhs : rhs;
+        // displaying comparison
+        style.setProperty("--color2", red);
     }
     return total;
 }
